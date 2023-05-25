ed21373be301a1747aa37deae1fb9f31f6f82e87
donaldxdonald <mzhlovefama@gmail.com>
2023-05-25 22:45:38 +0800

    style: update layout for social links

--- app/page.tsx
+++ app/page.tsx
@@ -34,22 +34,21 @@ export default async function Home() {
           }
         </Balancer>
       </p>
-      {
-        SocialLinks.map((item, index) => (
-          <a
-            key={item.link}
-            href={item.link}
-            target="_blank"
-            rel="noreferrer"
-            className="animate-fade-up mx-2 opacity-0 text-base md:text-xl mb-3 flex max-w-fit items-center font-display text-purple-900 justify-center space-x-2 overflow-hidden rounded-md px-4 py-2 transition-colors hover:bg-purple-200/70"
-            style={{ animationDelay: `${0.7 + index * 0.2}s`, animationFillMode: "forwards" }}
-          >
-            <item.icon className="w-5 h-5" />
-            <span>
-              {item.text}
-            </span>
-          </a>))
-      }
+      <ul className="flex items-center justify-center">
+        {
+          SocialLinks.map((item, index) => (
+            <a
+              key={item.link}
+              href={item.link}
+              target="_blank"
+              rel="noreferrer"
+              className="animate-fade-up mx-2 opacity-0 text-base md:text-xl mb-3 flex max-w-fit items-center font-display text-purple-900 justify-center space-x-2 overflow-hidden rounded-md p-2 transition-colors hover:bg-purple-200/70"
+              style={{ animationDelay: `${0.7 + index * 0.2}s`, animationFillMode: "forwards" }}
+            >
+              <item.icon className="w-5 h-5" />
+            </a>))
+        }
+      </ul>
     </div>
   )
 }
