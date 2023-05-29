df99059f4771950bfa0616e22bd11df6445d602e
donaldxdonald <mzhlovefama@gmail.com>
2023-05-30 01:10:41 +0800

    chore: add license link

--- components/layout/post.tsx
+++ components/layout/post.tsx
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs'
 import { useMDXComponent } from 'next-contentlayer/hooks'
 import { notFound } from 'next/navigation'
+import Link from 'next/link'
 import Balancer from 'react-wrap-balancer'
 import { DocumentTypes } from '../../.contentlayer/generated/types'
 
@@ -13,10 +14,12 @@ export default function Post({ post }: {post: DocumentTypes | undefined}) {
 
   return (
     <section className='flex flex-col mt-16 mb-56 w-[90%]'>
-      <h1 className="font-bold text-3xl font-serif tracking-tighter mt-5">
+      <h1 className="font-bold text-4xl font-serif tracking-tighter mt-5">
         <Balancer>{post.title}</Balancer>
       </h1>
-      <span className='text-md text-gray-400 my-3 py-2 border-t border-purple-900 w-fit font-mono'>{dayjs(post.date).format('YYYY-MM-DD hh:mm')}</span>
+      <span className='text-md text-gray-400 my-3 py-2 border-t border-purple-900 w-fit font-mono'>
+        {dayjs(post.date).format('YYYY-MM-DD hh:mm')} | (<Link className='hover:underline' target='__blank' href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>license</Link>)
+      </span>
       <article className='prose prose-slate max-w-3xl font-display mt-5'>
         <MDX></MDX>
       </article>
